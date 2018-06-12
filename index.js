ef1c05444f3c9968c00208c87e47994751e8d8c6
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-06-11 23:17:53 -0700

    finished as much of lab08

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 
-const server = require('./src/app.js');
+// const server = require('./src/app.js');
 
-server.start( dotenv, () => {
-  console.log(`Server is up on PORT: ${dotenv}`);
-});
+// server.start( process.env.PORT );
+
+// This will require our "app.js" file and immediately call its 'start' method, sending the port from our .env
+require('./src/app.js').start(process.env.PORT);
+
+
+//we arent storing things here in the form of const because all the work is happening in the expression. we dont need to store it in any variables
